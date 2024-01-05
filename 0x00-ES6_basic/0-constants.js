b775482f0d120adafd2f69aea8d75f3b90a8c14d
PeterEkwere <peterekwere007@gmail.com>
2024-01-05 10:37:29 +0000

    initial commit for task 0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+
+
+export function taskFirst() {
+	const task = 'I prefer const when I can.';
+	return task;
+}
+
+export function getLast() {
+	return ' is okay';
+}
+
+export function taskNext() {
+	let combination = 'But sometimes let';
+	combination += getLast();
+	
+	return combination;
+}
