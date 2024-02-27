a4ca47ad24027d1284a004b99e40a267acd33bee
PeterEkwere <peterekwere@icloud.com>
2024-02-28 00:47:27 +0100

    update 1 for task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-process.stdin.setEncoding('utf8');
